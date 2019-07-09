e9fc9e81552399bc0cc67ecbe2e04eddb194eb68
Mirochiu <mirochiu@cht.com.tw>
2019-07-09 10:24:57 +0800

    Add a webview component with transparent background

--- app/src/main/java/tw/idv/mirochiu/transparentbgapp/BarActivity.java
+++ app/src/main/java/tw/idv/mirochiu/transparentbgapp/BarActivity.java
@@ -1,11 +1,20 @@
 package tw.idv.mirochiu.transparentbgapp;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class BarActivity extends AppCompatActivity {
 
@@ -39,5 +48,32 @@ public class BarActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        WebView webview = (WebView) findViewById(R.id.web_view);
+        webview.setBackgroundColor(Color.TRANSPARENT);
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setAppCachePath(getDir("svc-appcache", 0).getPath());
+        webSettings.setAppCacheEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setDefaultTextEncodingName("UTF-8");
+        try {
+            InputStream in = getAssets().open("demo.html");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            StringBuilder htmlBuilder = new StringBuilder();
+            String str;
+            while ((str = reader.readLine()) != null) {
+                htmlBuilder.append(str);
+            }
+            in.close();
+            // Chinese text error
+            //webview.loadData(htmlBuilder.toString(), "text/html", "UTF-8");
+            // Chinese text okay
+            webview.loadDataWithBaseURL("", htmlBuilder.toString(), "text/html", "UTF-8",null);
+        } catch (IOException e) {
+            Toast.makeText(getApplicationContext(), String.format("Error:%s",e.getMessage()), Toast.LENGTH_SHORT);
+            e.printStackTrace();
+        }
     }
 }
